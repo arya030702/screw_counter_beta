751cbc768eafdb75c85f753b61e540e157561cfa
Rifqifdl <98653387+Rifqifdl@users.noreply.github.com>
2023-12-10 21:32:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 st.title('Deteksi dan Hitung Sekrup dengan YOLOv5')
-st.title('Kelompok 4 PCD- Pagi B')
+st.write('Kelompok 4 PCD- Pagi B')
 
 #region Functions
 # --------------------------------------------
